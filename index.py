ee9a92b8b35fe31259e1271818f86207c60aefab
EmanuelCav <cavallinema@gmail.com>
2023-02-15 22:00:01 -0300

    Matplotlib added

--- index.py
+++ index.py
@@ -1,17 +1,29 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=[13, 6])
+plt.style.use("ggplot")
 
 df = pd.read_excel("countries.xlsx")
 
 # Calculate the population density of each country
 
-pupulation = df["Population(2000-2005-2010-2015-2020)"].str.split(" ")
+population = df["Population(2000-2005-2010-2015-2020)"].str.split(" ")
+df["Population(2000-2005-2010-2015-2020)"] = population
+populationInt = []
 
-df["Population(2000-2005-2010-2015-2020)"] = pupulation
+for i, v in df.iterrows():
+    intPopulation = []
+    for j in range(0, 5):
+        intPopulation.append(int(v["Population(2000-2005-2010-2015-2020)"][j]))
+    populationInt.append(intPopulation)
+
+df["Population(2000-2005-2010-2015-2020)"] = populationInt
 
 lastPopulation = []
 
 for i, v in df.iterrows():
-    lastPopulation.append(int(v["Population(2000-2005-2010-2015-2020)"][4]))
+    lastPopulation.append(v["Population(2000-2005-2010-2015-2020)"][4])
 
 df["Population Density"] = lastPopulation/df["Surface"]
 
@@ -21,11 +33,26 @@ countCountriesPerContinent = df.groupby(["Continent"])["Country"].count()
 sumCountries = countCountriesPerContinent.sum()
 percentage = (countCountriesPerContinent*100)/sumCountries
 
+amountCountriesperContinent = []
+
+for i in range(0, countCountriesPerContinent.shape[0]):
+    amountCountriesperContinent.append(countCountriesPerContinent.iloc[i])
+
+plt.pie(x=amountCountriesperContinent, labels=sorted(df["Continent"].unique()), autopct="%.2f%%")
+plt.title("Percentage of countries per continent")
+plt.show()
+
 # Countries bigger than Chile in surface
 
 chileIndex = df.loc[df["Country"] == "Chile"].index[0]
 chileSurface = df.iloc[chileIndex]["Surface"]
-df.loc[df["Surface"] > chileSurface].reset_index(drop=True)
+biggerThanChile = df.loc[df["Surface"] > chileSurface].reset_index(drop=True)
+
+plt.barh(biggerThanChile["Country"], biggerThanChile["Surface"], color="y")
+plt.xlabel("Surface (km^2)")
+plt.ylabel("Countries")
+plt.title("Countries bigger than Chile in surface")
+plt.show()
 
 # Countries where the population is declining
 
@@ -39,17 +66,41 @@ for i, v in df.iterrows():
 
 df["Population Declining"] = isPopulationDeclining
 
+decliningPopCountries = df.loc[df["Population Declining"]]
+
+for i, v in decliningPopCountries.iterrows():
+    plt.plot([2000, 2005, 2010, 2015, 2020], v["Population(2000-2005-2010-2015-2020)"], marker='o', color="b", label=v["Country"])
+
+plt.title("Countries where the population is declining")
+plt.xlabel("Years")
+plt.ylabel("Population")
+plt.xticks([2000, 2005, 2010, 2015, 2020])
+plt.legend(loc="lower left")
+plt.show()
+
 # Percentaje of surface of each country in South America
 
 southAmerica = df.loc[df["Region"] == "South America"].reset_index(drop=True)
 totalSurface = southAmerica["Surface"].sum()
 southAmerica["Percentage Surface"] = (southAmerica["Surface"]*100)/totalSurface
 
+plt.pie(x=southAmerica["Surface"], labels=southAmerica["Country"], shadow=True, autopct='%.2f%%')
+plt.title("Percentage of surface of each country in South America")
+plt.axis("equal")
+plt.show()
+
 # The 5 largest landlocked countries
 
 landlockedCountries = df.loc[df["Coastline"] == 0].reset_index(drop=True)
 landlockedCountries = landlockedCountries.sort_values("Surface", ascending=False)
 landlockedCountries.head()
+
+plt.barh(landlockedCountries["Country"], landlockedCountries["Surface"], color="g")
+plt.xlabel("Surface (km^2)")
+plt.ylabel("Countries")
+plt.title("Largest Landlocked Countries")
+plt.xticks([0, 300000, 1000000, 3000000])
+plt.show()
 
 # What is more in Africa?. Christian countries or Islam countries
 
@@ -67,7 +118,7 @@ lastPopulation = []
 df["Languages"] = df["Languages"].str.split(' ')
 
 for i, v in df.iterrows():
-    lastPopulation.append(int(v["Population(2000-2005-2010-2015-2020)"][4]))
+    lastPopulation.append(v["Population(2000-2005-2010-2015-2020)"][4])
     mainLanguages.append(v["Languages"][0])
 
 df["Main Language"] = mainLanguages
@@ -75,7 +126,7 @@ df["Population"] = lastPopulation
 populationForLanguage = df.groupby(["Main Language"]).agg({
     "Population": "sum",
 })
-populationForLanguage.sort_values("Population", ascending=False)
+populationForLanguage = populationForLanguage.sort_values("Population", ascending=False)
 
 # Largest country per region
 
@@ -90,8 +141,4 @@ for i in range(0, df["Region"].nunique()):
 # Islands that are not dependents countries
 
 islands = df.loc[df["Land Borders"] == 0].reset_index(drop=True)
-islands.loc[islands["Independence Day"] != "Dependent"]
-
-
-
-
+independentsIslands = islands.loc[islands["Independence Day"] != "Dependent"]
